406f1598471b435c67b650dc0012f71654f4a276
Vlad Tsvang <vtsvang@gmail.com>
2015-03-25 20:19:26 -0700

    Added GridWorld

---
+++ GridWorld/src/firstProject/WildBug.java
@@ -0,0 +1,23 @@
+package firstProject;
+
+import info.gridworld.actor.Bug;
+import java.awt.Color;
+import java.util.Random;
+
+public class WildBug extends Bug {
+  private Random rand = new Random();
+  
+  
+  public WildBug() {
+    super();
+  }
+
+  @Override
+  public void act() {
+    for (int i = 0; i < rand.nextInt(4); i++) {
+      turn();
+    }
+
+    super.act();
+  }
+}
